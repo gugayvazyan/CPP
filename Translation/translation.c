f2585936a911d25b4e653bf10956c59979439758
Gurgen Ayvazyan <ayvazyangurgen777@gmail.com>
2025-10-02 19:16:27 +0400

    Create translation.c

---
+++ Translation/translation.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct{
+    char name[50];
+    int age;
+    double height;
+} Person;
+
+// Constructors
+void Person_ctor(Person* const this){
+	this->age = 0;
+	this->height = 0.0;
+    strcpy(this->name, "Unknown");
+}
+
+void Person_ctor_i(Person* const this, char* n, int a, double h){
+	this->age = a;
+	this->height = h;
+    strcpy(this->name, n);
+}
+
+// Getters
+const char* getName(Person* const this) { return this->name; }
+const int getAge(Person* const this) { return this->age; }
+const double getHeight(Person* const this) { return this->height; }
+
+void setName(Person* const this,const char* n) { strcpy(this->name, n); }
+void setAge(Person* const this,int a) { this->age = a; }
+void setHeight(Person* const this,double h) { this->height = h; }
+
+// Overloaded member functions
+void printInfo(Person* const this) { printf("Name: %s, Age: %d, Height: %0.2f\n", this->name, this->age, this->height); }
+
+void printInfo_b(Person* const this,bool showHeight) {
+    printf("Name: %s, Age: %d", this->name, this->age);
+    if(showHeight) printf(", Height: %0.2f", this->height);
+    printf("\n");
+}
+
+void Person_dtor(Person* const this) {}
+
+int main() {
+    Person p1;
+    Person_ctor_i(&p1,"Bob", 19, 1.75);
+
+    printInfo(&p1);
+    printInfo_b(&p1,false);
+
+    setAge(&p1,20);
+    
+    printf("Updates age: %d\n", getAge(&p1));
+    return 0;
+    Person_dtor(&p1);
+}
